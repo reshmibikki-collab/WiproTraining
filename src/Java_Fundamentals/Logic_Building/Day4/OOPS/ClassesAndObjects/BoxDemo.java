b9064ed5606db2cf337ba97966d63c7b4885b85c
Reshmitha Bikki <reshmibikki@example.com>
2026-07-10 15:13:50 +0530

    Added OOPS Classes and Objects programs

---
+++ src/Java_Fundamentals/Logic_Building/Day4/OOPS/ClassesAndObjects/BoxDemo.java
@@ -0,0 +1,26 @@
+package Java_Fundamentals.Logic_Building.Day4.OOPS.ClassesAndObjects;
+
+class Box {
+    double width;
+    double height;
+    double depth;
+
+    Box(double w, double h, double d) {
+        width = w;
+        height = h;
+        depth = d;
+    }
+
+    double getVolume() {
+        return width * height * depth;
+    }
+}
+
+public class BoxDemo {
+    public static void main(String[] args) {
+        Box myBox = new Box(10, 20, 15);
+        System.out.println("The volume of the box is: " + myBox.getVolume());
+    }
+}
+    
+
