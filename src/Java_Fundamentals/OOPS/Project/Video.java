3d4125bb5d4f020aa49b2e4eb03b500584fe8304
Reshmitha Bikki <reshmibikki@example.com>
2026-07-16 19:04:01 +0530

    Added OOPS Video Store project

---
+++ src/Java_Fundamentals/OOPS/Project/Video.java
@@ -0,0 +1,45 @@
+package Java_Fundamentals.OOPS.Project;
+
+public class Video {
+
+    private String videoName;
+    private boolean checkout;
+    private int rating;
+
+    // Constructor
+    public Video(String name) {
+        this.videoName = name;
+        this.checkout = false;
+        this.rating = 0;
+    }
+
+    // Get video name
+    public String getName() {
+        return videoName;
+    }
+
+    // Checkout video
+    public void doCheckout() {
+        checkout = true;
+    }
+
+    // Return video
+    public void doReturn() {
+        checkout = false;
+    }
+
+    // Receive rating
+    public void receiveRating(int rating) {
+        this.rating = rating;
+    }
+
+    // Get rating
+    public int getRating() {
+        return rating;
+    }
+
+    // Check checkout status
+    public boolean getCheckout() {
+        return checkout;
+    }
+}
