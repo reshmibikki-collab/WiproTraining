73789f9fa8be2486d16c69066815e60a7b7027c4
Reshmitha Bikki <reshmibikki@example.com>
2026-07-16 15:53:58 +0530

    Added Java Collections projects 1 to 6

---
+++ src/Java_Fundamentals/Collections/Project3/Card.java
@@ -0,0 +1,49 @@
+package Java_Fundamentals.Collections.Project3;
+
+public class Card implements Comparable<Card> {
+
+    private String symbol;
+    private int number;
+
+    public Card(String symbol, int number) {
+        this.symbol = symbol;
+        this.number = number;
+    }
+
+    public String getSymbol() {
+        return symbol;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+
+        if (this == obj)
+            return true;
+
+        if (!(obj instanceof Card))
+            return false;
+
+        Card c = (Card) obj;
+
+        return this.symbol.equals(c.symbol);
+    }
+
+    @Override
+    public int hashCode() {
+        return symbol.hashCode();
+    }
+
+    @Override
+    public int compareTo(Card c) {
+        return this.symbol.compareTo(c.symbol);
+    }
+
+    @Override
+    public String toString() {
+        return symbol + " " + number;
+    }
+}
