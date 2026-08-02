a35cd1978ad88ba458891613d2e8b41691b4868c
Reshmitha Bikki <reshmibikki@example.com>
2026-08-02 17:13:55 +0530

    Added employee alias query

---
+++ Oracle_SQl/03_Employee_Alias.sql
@@ -0,0 +1,9 @@
+-- Display employee details with column aliases
+
+SELECT 
+    employee_id AS "Emp #", 
+    last_name AS "Employee", 
+    job_id AS "Job", 
+    hire_date AS "Hire Date"
+FROM 
+    employees;
